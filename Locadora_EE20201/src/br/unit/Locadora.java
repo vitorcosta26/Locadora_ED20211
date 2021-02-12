34ada84a879027732650f66015804defc6d7a2c0
vitorcosta26 <60762180+vitorcosta26@users.noreply.github.com>
2021-02-12 16:44:44 -0300

    Update Locadora.java

--- Locadora_EE20201/src/br/unit/Locadora.java
+++ Locadora_EE20201/src/br/unit/Locadora.java
@@ -1,32 +1,41 @@
 package br.unit;
 // Questão 04.
 public class Locadora {
+	
 	// Item a.
 	private Disco[] listaDeDiscos = new Disco[2];
+	
 	// Item b.
 	private Filme[] listaDeFilmes = new Filme[2];
+	
 	// Item c.
 	private int indiceD;
+	
 	// Item d.
 	private int indiceF;
-    	// Item e.
+    	
+	// Item e.
     	public Locadora() {
     	}
-    	// Item f.
+    	
+	// Item f.
     	public void novoDisco(Disco d) {
         	this.listaDeDiscos[this.indiceD++] = d;
     	}
-    	// Item g.
+    	
+	// Item g.
     	public void novoFilme(Filme f) {
         	this.listaDeFilmes[this.indiceF++] = f;
     	}
-    	// Item h.
+    	
+	// Item h.
     	public void listarDiscos() {
         	for (Disco disco : listaDeDiscos) {
             	disco.ListaInformacoes();
         	}
     	}
-    	// Item i.
+    	
+	// Item i.
     	public void listarFilmes() {
         	for (Filme filme : listaDeFilmes) {
             	filme.ListaInformacoes();
