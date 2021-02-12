5754f7b6c0223032f2c9c2dd48e7842d6e85e625
vitorcosta26 <60762180+vitorcosta26@users.noreply.github.com>
2021-02-11 21:34:44 -0300

    Update UsaLocadora.java

--- Locadora_EE20201/src/br/unit/UsaLocadora.java
+++ Locadora_EE20201/src/br/unit/UsaLocadora.java
@@ -6,25 +6,25 @@ public class UsaLocadora {
 		
 		Locadora aluguel = new Locadora();
 		
-		// Atribuindo Informações do Disco 1.
-		Disco disco1 = new Disco("Samba Social Clube 5", 2014, "Vários Sambas", "Vários Artistas", 12, "CD");
-	    // Atribuindo Informações do Disco 2.
+		// Atribuindo InformaÃ§Ãµes do Disco 1.
+		Disco disco1 = new Disco("Samba Social Clube 5", 2014, "VÃ¡rios Sambas", "VÃ¡rios Artistas", 12, "CD");
+	    	// Atribuindo InformaÃ§Ãµes do Disco 2.
 		Disco disco2 = new Disco("Pode Entrar", 2009, "Ivete e Convidados", "Ivete Sangalo", 14, "DVD");
 		// Instanciando os objetos discos a classe Disco.
 		aluguel.novoDisco(disco1);
-	    aluguel.novoDisco(disco2);
-	    // Listando as informaçoes.
-	    aluguel.listarDiscos();
+	    	aluguel.novoDisco(disco2);
+	    	// Listando as informaÃ§oes.
+	    	aluguel.listarDiscos();
 		
-		// Atribuindo Informações do Filme 1.
-	    Filme filme1 = new Filme("Smurfs 2", 2013, "Gargamel cria a versão malvada dos Smurfs", "Raja Gosnell", 140, "Animação");
-	    // Atribuindo Informações do Filme 2
-	    Filme filme2 = new Filme("Os mercenários 2", 2012, "De volta ao mundo", "Simon West", 210, "Animação");
-	    // Instanciando os objetos filmes a classe Filme.
-	    aluguel.novoFilme(filme1);
-	    aluguel.novoFilme(filme2);
-	    // Listando as informaçoes.
-	    aluguel.listarFilmes();
+		// Atribuindo InformaÃ§Ãµes do Filme 1.
+	    	Filme filme1 = new Filme("Smurfs 2", 2013, "Gargamel cria a versÃ£o malvada dos Smurfs", "Raja Gosnell", 140, "AnimaÃ§Ã£o");
+	    	// Atribuindo InformaÃ§Ãµes do Filme 2
+	    	Filme filme2 = new Filme("Os mercenÃ¡rios 2", 2012, "De volta ao mundo", "Simon West", 210, "AnimaÃ§Ã£o");
+	    	// Instanciando os objetos filmes a classe Filme.
+	    	aluguel.novoFilme(filme1);
+	    	aluguel.novoFilme(filme2);
+	    	// Listando as informaÃ§oes.
+	    	aluguel.listarFilmes();
 
 	}
 }
